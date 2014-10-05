a90c9a016ac07ba4e913fd8bb61f98046f3db5af
Jordan Evans <jordane@osuosl.org>
2014-10-04 17:09:45 -0700

    commit bare cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'jordane-cookbook-test'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures jordane-cookbook-test'
+long_description 'Installs/Configures jordane-cookbook-test'
+version          '0.1.0'
+
